5c81ef90d37019f342d7f2f2b668828a0c03dc73
Dutchman007 <tosinkazeem77@gmail.com>
2025-07-24 16:50:07 +0100

    The catalogue of courses a student is taking in his first year

--- task4.py
+++ task4.py
@@ -10,3 +10,19 @@
     4. Find and display the total number of courses the student is now registered for.
     5. The student wants to know what their 3rd course in the list is.
 """
+
+courses = ["MTH 101", "PHY 101", "CHM 101", "CSC 101", "GST 101"]
+print(courses)
+
+courses.insert(0, "ENG 101")
+print(courses)
+
+courses.remove("GST 101")
+print(courses)
+
+courses.insert(4, "BIO 101")
+print(courses)
+
+print("The total number of courses the student is taking is:", len(courses))
+
+print("The third course in the student's list is:", courses[2])
